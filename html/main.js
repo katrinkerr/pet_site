a0e6d17ce278f3e93b6fb72678301e3130bf0c4e
katrinkaia <katya2009.savchenko@gmail.com>
2026-08-19 21:42:24 +0300

    added form, dropdown

--- html/main.js
+++ html/main.js
@@ -5,5 +5,20 @@ circles.forEach(circle=>{
         circles.forEach(c=>c.classList.remove("selected"));
         circle.classList.add("selected");
         selectedFriend=circle.dataset.friend;
+        openForm.disabled=false;
     })
 })
+document.getElementById("openForm").addEventListener("click",()=>{
+    document.getElementById("petForm").style.display="block";
+    form.classList.add("visible");
+})
+
+const icon=document.getElementById('menu-icon');
+const menu=document.getElementById('menu')
+icon.addEventListener("click",()=>{
+    if(menu.style.display=="none"){
+        menu.style.display="block"
+    }
+    else{
+        menu.style.display="none"
+    }})
